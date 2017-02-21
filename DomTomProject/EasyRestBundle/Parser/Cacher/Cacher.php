77bf707420daf048ec47660c88367fb4656e0abc
Damian Zschille <crunkowiec@gmail.com>
2017-02-21 15:01:10 +0100

    Parser in progress

--- DomTomProject/EasyRestBundle/Parser/Cacher/Cacher.php
+++ DomTomProject/EasyRestBundle/Parser/Cacher/Cacher.php
@@ -4,14 +4,66 @@ namespace DomTomProject\EasyRestBundle\Parser\Cacher;
 
 class Cacher implements CacherInterface {
 
+    /**
+     *
+     * @var string 
+     */
+    private $env;
+
+    /**
+     *
+     * @var string
+     */
+    private $cacheDir;
+
+    /**
+     * 
+     * @param string $env
+     * @param string $cacheDir
+     */
+    public function __construct(string $env, string $cacheDir) {
+        $this->env = $env;
+        $this->cacheDir = $cacheDir;
+    }
+
+    /**
+     * 
+     * @param string $file
+     * @param bool $absolute
+     * @return array
+     */
     public function getCache(string $file, bool $absolute = false): array {
-        
+        $filename = $this->cacheDir . '/validation/' . $file . '.php';
+        if (!file_exists($filename)) {
+            return [];
+        }
+
+        return require $filename;
     }
 
+    /**
+     * 
+     * @param string $file
+     * @param bool $absolute
+     * @return bool
+     */
     public function isCached(string $file, bool $absolute = false): bool {
-        
+        if ($this->env === 'dev') {
+            return false;
+        }
+
+        if (file_exists($this->cacheDir . '/validation/' . $file . '.php')) {
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * 
+     * @param string $file
+     * @param array $data
+     * @param bool $absolute
+     */
     public function save(string $file, array $data, bool $absolute = false) {
         
     }
--- DomTomProject/EasyRestBundle/Parser/YamlRulesParser.php
+++ DomTomProject/EasyRestBundle/Parser/YamlRulesParser.php
@@ -2,15 +2,11 @@
 
 namespace DomTomProject\EasyRestBundle\Parser;
 
-use DomTomProject\EasyRestBundle\Provider\CacherProvider;
-use DomTomProject\EasyRestBundle\Parser\Cacher\CacherInterface;
+use Symfony\Component\Yaml\Yaml;
+use DomTomProject\EasyRestBundle\Exception\RulesFileNotFoundException;
+use DomTomProject\EasyRestBundle\Exception\RulesKeyNotFoundException;
 
 class YamlRulesParser implements RulesParserInterface {
-
-    /**
-     * @var CacherInterface 
-     */
-    private $cacher;
 
     /**
      *
@@ -23,17 +19,120 @@ class YamlRulesParser implements RulesParserInterface {
      * @param CacherProvider $provider
      * @param string $validationPath
      */
-    public function __construct(CacherProvider $provider, string $validationPath) {
-        $this->cacher = $provider->provide();
+    public function __construct(string $validationPath) {
         $this->validationPath = $validationPath;
     }
 
     /**
-     * 
+     * @param string $name
+     * @param string $key
      * @return array
+     * @throws RulesKeyNotFoundException
      */
     public function parse(string $name, string $key): array {
-        return [$name => $key];
+        $filename = $this->getFilename($name);
+        $rules = Yaml::parse($this->getFile($name));
+
+        if (!isset($rules[$key])) {
+            throw new RulesKeyNotFoundException('Key ' . $key . ' not found in ' . $name . ' rules file.');
+        }
+
+        //// tutaj będzie się działa magia
+        dump($this->generateRules($rules[$key]));
+
+
+        die();
+
+        return $rules;
+    }
+
+    /**
+     * 
+     * @param array $rulesGroups
+     */
+    private function generateRules(array $rulesGroups): array {
+        $rules = [];
+        foreach ($rulesGroups as $key => $group) {
+            $rules[$key] = $this->generateRuleForGroup($group);
+        }
+
+        return $rules;
+    }
+
+    /**
+     * 
+     * @param array $group
+     * @return string
+     */
+    private function generateRuleForGroup(array $group): string {
+        $first = true;
+        $generated = '';
+
+        foreach ($group as $rules) {
+            if (!$first) {
+                $generated .= '->';
+            }
+            $first = false;
+
+            // method with no arguments, ex: stringType
+            if (!is_array($rules)) {
+                $generated .= $rules . '()';
+                continue;
+            }
+
+            $generated .= $this->generateFunctionWithArguments($rules);
+        }
+
+        return $generated;
+    }
+
+    /**
+     * 
+     * @param string $functionName
+     * @param array $arguments
+     */
+    private function generateFunctionWithArguments(array $arguments): string {
+        // get function name
+        $functionName = current(array_keys($arguments));
+        $function = $functionName . '(';
+        $first = true;
+
+        // build args for this functions
+        foreach ($arguments[$functionName] as $argument) {
+            if (!$first) {
+                $function .= ', ';
+            }
+            $first = false;
+
+            if (!is_array($argument)) {
+                $function .= $argument;
+            }
+        }
+
+        return $function . ')';
+    }
+
+    /**
+     * 
+     * @param string $name
+     * @return string
+     * @throws RulesFileNotFoundException
+     */
+    private function getFile(string $name): string {
+        $filename = $this->getFilename($name);
+        if (!file_exists($filename)) {
+            throw new RulesFileNotFoundException('Validation file not found in: ' . $filename);
+        }
+
+        return file_get_contents($filename);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private function getFilename(string $name): string {
+        return $this->validationPath . '/' . $name . '.yml';
     }
 
 }
