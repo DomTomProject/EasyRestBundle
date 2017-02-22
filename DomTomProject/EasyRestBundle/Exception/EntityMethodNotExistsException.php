04b1a4e67eb4c6522984e7165154487a4aff6bf0
Damian Zschille <crunkowiec@gmail.com>
2017-02-22 12:41:52 +0100

    Trait changed.

---
+++ DomTomProject/EasyRestBundle/Exception/EntityMethodNotExistsException.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace DomTomProject\EasyRestBundle\Exception;
+
+use Exception;
+use Throwable;
+
+/**
+ * 
+ *  @author Damian Zschille <crunkowiec@gmail.com>
+ */
+class EntityMethodNotExistsException extends Exception{
+
+    public function __construct(string $message = "", int $code = 500, Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
